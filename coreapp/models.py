58bf0cd149116210924f0d52c6523888656ee977
hbp_02 <hector.bolsa@outlook.com>
2018-12-24 07:57:53 +0000

    Custom User model by extending AbstractUser model.

--- coreapp/models.py
+++ coreapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+# Extending AbstractUser to create a custom user model
+from django.contrib.auth.models import AbstractUser
+class User (AbstractUser):
+    date_left = models.CharField(max_length=30)
+    projects = models.CharField(max_length=30)
+    author_of = models.CharField(max_length=30)
+    def __str__(self):
+        return self.get_fullname()
+
