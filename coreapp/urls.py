ac13cbe875b34140694dba0bbf35de315a5cc57d
hbp_01 <hector.bolsa@outlook.com>
2018-12-26 19:44:15 +0000

    Urls and views scaffolding

--- coreapp/urls.py
+++ coreapp/urls.py
@@ -3,4 +3,40 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    
+    path('project/', views.project_index, name='project-index'),
+    path('project/create/', views.project_create, name='project-create'),
+    path('project/edit/<int:id>', views.project_edit, name='project-edit'),
+    path('project/edit/update/<int:id>', views.project_update, name='project-update'),
+    path('project/delete/<int:id>', views.project_delete, name='project-delete'),
+    
+    path('project/', views.location_index, name='location-index'),
+    path('location/create/', views.location_create, name='location-create'),
+    path('location/edit/<int:id>', views.location_edit, name='location-edit'),
+    path('location/edit/update/<int:id>', views.location_update, name='location-update'),
+    path('location/delete/<int:id>', views.location_delete, name='location-delete'),
+    
+    path('studyplan/', views.studyplan_index, name='studyplan-index'),
+    path('studyplan/create/', views.studyplan_create, name='studyplan-create'),
+    path('studyplan/edit/<int:id>', views.studyplan_edit, name='studyplan-edit'),
+    path('studyplan/edit/update/<int:id>', views.studyplan_update, name='studyplan-update'),
+    path('studyplan/delete/<int:id>', views.studyplan_delete, name='studyplan-delete'),
+    
+    path('report/', views.report_index, name='report-index'),
+    path('report/create/', views.report_create, name='report-create'),
+    path('report/edit/<int:id>', views.report_edit, name='report-edit'),
+    path('report/edit/update/<int:id>', views.report_update, name='report-update'),
+    path('report/delete/<int:id>', views.report_delete, name='report-delete'),
+    
+    path('relatedfile/', views.relatedfile_index, name='relatedfile-index'),
+    path('relatedfile/create/', views.relatedfile_create, name='relatedfile-create'),
+    path('relatedfile/edit/<int:id>', views.relatedfile_edit, name='relatedfile-edit'),
+    path('relatedfile/edit/update/<int:id>', views.relatedfile_update, name='relatedfile-update'),
+    path('relatedfile/delete/<int:id>', views.relatedfile_delete, name='relatedfile-delete'),
+    
+    path('labbook/', views.labbook_index, name='labbook-index'),
+    path('labbook/create/', views.labbook_create, name='labbook-create'),
+    path('labbook/edit/<int:id>', views.labbook_edit, name='labbook-edit'),
+    path('labbook/edit/update/<int:id>', views.labbook_update, name='labbook-update'),
+    path('labbook/delete/<int:id>', views.labbook_delete, name='labbook-delete'),
 ]
--- coreapp/views.py
+++ coreapp/views.py
@@ -3,3 +3,76 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'coreapp/home.html')
 
+# Project views
+def project_index(request):
+    pass
+def project_create(request):
+    pass
+def project_edit(request):
+    pass
+def project_update(request):
+    pass
+def project_delete(request):
+    pass
+
+# Location views
+def location_index(request):
+    pass
+def location_create(request):
+    pass
+def location_edit(request):
+    pass
+def location_update(request):
+    pass
+def location_delete(request):
+    pass
+
+# StudyPlan views
+def studyplan_index(request):
+    pass
+def studyplan_create(request):
+    pass
+def studyplan_edit(request):
+    pass
+def studyplan_update(request):
+    pass
+def studyplan_delete(request):
+    pass
+
+# Report views
+def report_index(request):
+    pass
+def report_create(request):
+    pass
+def report_edit(request):
+    pass
+def report_update(request):
+    pass
+def report_delete(request):
+    pass
+
+# RelatedFile views
+def relatedfile_index(request):
+    pass
+def relatedfile_create(request):
+    pass
+def relatedfile_edit(request):
+    pass
+def relatedfile_update(request):
+    pass
+def relatedfile_delete(request):
+    pass
+
+# LabBook views
+def labbook_index(request):
+    pass
+def labbook_create(request):
+    pass
+def labbook_edit(request):
+    pass
+def labbook_update(request):
+    pass
+def labbook_delete(request):
+    pass
+
+
