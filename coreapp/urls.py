a241586e6481b9d465884b165ababca1fb37b5f0
hbp_02 <hector.bolsa@outlook.com>
2018-12-27 15:52:30 +0000

    Index files renamed. Lists.

--- coreapp/urls.py
+++ coreapp/urls.py
@@ -4,37 +4,37 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     
-    path('project/', views.project_index, name='project-index'),
+    path('project/', views.project_list, name='project-list'),
     path('project/create/', views.project_create, name='project-create'),
     path('project/edit/<int:id>', views.project_edit, name='project-edit'),
     path('project/edit/update/<int:id>', views.project_update, name='project-update'),
     path('project/delete/<int:id>', views.project_delete, name='project-delete'),
     
-    path('location/', views.location_index, name='location-index'),
+    path('location/', views.location_list, name='location-list'),
     path('location/create/', views.location_create, name='location-create'),
     path('location/edit/<int:id>', views.location_edit, name='location-edit'),
     path('location/edit/update/<int:id>', views.location_update, name='location-update'),
     path('location/delete/<int:id>', views.location_delete, name='location-delete'),
     
-    path('studyplan/', views.studyplan_index, name='studyplan-index'),
+    path('studyplan/', views.studyplan_list, name='studyplan-list'),
     path('studyplan/create/', views.studyplan_create, name='studyplan-create'),
     path('studyplan/edit/<int:id>', views.studyplan_edit, name='studyplan-edit'),
     path('studyplan/edit/update/<int:id>', views.studyplan_update, name='studyplan-update'),
     path('studyplan/delete/<int:id>', views.studyplan_delete, name='studyplan-delete'),
     
-    path('report/', views.report_index, name='report-index'),
+    path('report/', views.report_list, name='report-list'),
     path('report/create/', views.report_create, name='report-create'),
     path('report/edit/<int:id>', views.report_edit, name='report-edit'),
     path('report/edit/update/<int:id>', views.report_update, name='report-update'),
     path('report/delete/<int:id>', views.report_delete, name='report-delete'),
     
-    path('relatedfile/', views.relatedfile_index, name='relatedfile-index'),
+    path('relatedfile/', views.relatedfile_list, name='relatedfile-list'),
     path('relatedfile/create/', views.relatedfile_create, name='relatedfile-create'),
     path('relatedfile/edit/<int:id>', views.relatedfile_edit, name='relatedfile-edit'),
     path('relatedfile/edit/update/<int:id>', views.relatedfile_update, name='relatedfile-update'),
     path('relatedfile/delete/<int:id>', views.relatedfile_delete, name='relatedfile-delete'),
     
-    path('labbook/', views.labbook_index, name='labbook-index'),
+    path('labbook/', views.labbook_list, name='labbook-list'),
     path('labbook/create/', views.labbook_create, name='labbook-create'),
     path('labbook/edit/<int:id>', views.labbook_edit, name='labbook-edit'),
     path('labbook/edit/update/<int:id>', views.labbook_update, name='labbook-update'),
--- coreapp/views.py
+++ coreapp/views.py
@@ -1,78 +1,91 @@
 from django.shortcuts import render
+from coreapp.models import Project, Location, StudyPlan, Report, RelatedFile, LabBook
 
 def home(request):
     return render(request, 'coreapp/home.html')
 
 # Project views
-def project_index(request):
-    return render(request, 'coreapp/project_index.html')
+def project_list(request):
+    projects = Project.objects.all()
+    context = {'projects': projects, 'page_title': 'R&D EPI | Projects'}
+    return render(request, 'coreapp/project_list.html', context)
 def project_create(request):
     pass
-def project_edit(request):
+def project_edit(request, id):
     pass
-def project_update(request):
+def project_update(request, id):
     pass
-def project_delete(request):
+def project_delete(request, id):
     pass
 
 # Location views
-def location_index(request):
-    return render(request, 'coreapp/location_index.html')
+def location_list(request):
+    locations = Location.objects.all()
+    context = {'locations': locations, 'page_title': 'R&D EPI | Locations'}
+    return render(request, 'coreapp/location_list.html', context)
 def location_create(request):
     pass
-def location_edit(request):
+def location_edit(request, id):
     pass
-def location_update(request):
+def location_update(request, id):
     pass
-def location_delete(request):
+def location_delete(request, id):
     pass
 
 # StudyPlan views
-def studyplan_index(request):
-    return render(request, 'coreapp/studyplan_index.html')
+def studyplan_list(request):
+    studyplans = StudyPlan.objects.all()
+    context = {'studyplans': studyplans, 'page_title': 'R&D EPI | Study plans'}
+    return render(request, 'coreapp/studyplan_list.html', context)
 def studyplan_create(request):
     pass
-def studyplan_edit(request):
+def studyplan_edit(request, id):
     pass
-def studyplan_update(request):
+def studyplan_update(request, id):
     pass
-def studyplan_delete(request):
+def studyplan_delete(request, id):
     pass
 
 # Report views
-def report_index(request):
-    return render(request, 'coreapp/report_index.html')
+def report_list(request):
+    reports = Report.objects.all()
+    context = {'reports': reports, 'page_title': 'R&D EPI | Reports'}
+    return render(request, 'coreapp/report_list.html', context)
 def report_create(request):
     pass
-def report_edit(request):
+def report_edit(request, id):
     pass
-def report_update(request):
+def report_update(request, id):
     pass
-def report_delete(request):
+def report_delete(request, id):
     pass
 
 # RelatedFile views
-def relatedfile_index(request):
-    return render(request, 'coreapp/relatedfile_index.html')
+def relatedfile_list(request):
+    relatedfiles = RelatedFile.objects.all()
+    context = {'relatedfiles': relatedfiles, 'page_title': 'R&D EPI | Project-related files'}
+    return render(request, 'coreapp/relatedfile_list.html', context)
 def relatedfile_create(request):
     pass
-def relatedfile_edit(request):
+def relatedfile_edit(request, id):
     pass
-def relatedfile_update(request):
+def relatedfile_update(request, id):
     pass
-def relatedfile_delete(request):
+def relatedfile_delete(request, id):
     pass
 
 # LabBook views
-def labbook_index(request):
-    return render(request, 'coreapp/labbook_index.html')
+def labbook_list(request):
+    labbooks = LabBook.objects.all()
+    context = {'labbooks': labbooks, 'page_title': 'R&D EPI | Lab books'}
+    return render(request, 'coreapp/labbook_list.html', context)
 def labbook_create(request):
     pass
-def labbook_edit(request):
+def labbook_edit(request, id):
     pass
-def labbook_update(request):
+def labbook_update(request, id):
     pass
-def labbook_delete(request):
+def labbook_delete(request, id):
     pass
 
 
