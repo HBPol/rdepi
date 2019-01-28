185300a8149b8ad2dde3308494019a680a6ee330
hbp_01 <hector.bolsa@outlook.com>
2019-01-28 00:49:33 +0000

    'Edit' feature for all models.

--- coreapp/views.py
+++ coreapp/views.py
@@ -25,9 +25,19 @@ def project_create(request):
     return render (request, 'coreapp/project_create.html', context)
 
 def project_edit(request, id):
-    pass
+    project = Project.objects.get(id=id)
+    # Use instance of project to instantiate the from with initial values for the project with id=id
+    form = ProjectForm(instance=project)
+    context = {'project': project, 'form_project': form, 'page_title': 'R&D EPI | Edit project'}
+    return render(request, 'coreapp/project_edit.html', context)
+
 def project_update(request, id):
-    pass
+    project = Project.objects.get(id=id)
+    form = ProjectForm(request.POST or None, instance=project)
+    if form.is_valid():
+        form.save()
+        return redirect('project-list')
+    
 def project_delete(request, id):
     pass
 
@@ -48,9 +58,16 @@ def location_create(request):
     return render (request, 'coreapp/location_create.html', context)
 
 def location_edit(request, id):
-    pass
+    location = Location.objects.get(id=id)
+    form = LocationForm(instance=location)
+    context = {'location': location, 'form_location': form, 'page_title': 'R&D EPI | Edit location'}
+    return render(request, 'coreapp/location_edit.html', context)
 def location_update(request, id):
-    pass
+    location = Location.objects.get(id=id)
+    form = LocationForm(request.POST or None, instance=location)
+    if form.is_valid():
+        form.save()
+        return redirect('location-list')
 def location_delete(request, id):
     pass
 
@@ -70,9 +87,16 @@ def studyplan_create(request):
     context = {'form_studyplan': form, 'page_title': 'R&D EPI | Create new study plan'}
     return render (request, 'coreapp/studyplan_create.html', context)
 def studyplan_edit(request, id):
-    pass
+    studyplan = StudyPlan.objects.get(id=id)
+    form = StudyPlanForm(instance=studyplan)
+    context = {'studyplan': studyplan, 'form_studyplan': form, 'page_title': 'R&D EPI | Edit study plan'}
+    return render(request, 'coreapp/studyplan_edit.html', context)
 def studyplan_update(request, id):
-    pass
+    studyplan = StudyPlan.objects.get(id=id)
+    form = StudyPlanForm(request.POST, request.FILES, instance=studyplan)
+    if form.is_valid():
+        form.save()
+        return redirect('studyplan-list')
 def studyplan_delete(request, id):
     pass
 
@@ -92,9 +116,16 @@ def report_create(request):
     context = {'form_report': form, 'page_title': 'R&D EPI | Create new report'}
     return render (request, 'coreapp/report_create.html', context)
 def report_edit(request, id):
-    pass
+    report = Report.objects.get(id=id)
+    form = ReportForm(instance=report)
+    context = {'report': report, 'form_report': form, 'page_title': 'R&D EPI | Edit report'}
+    return render(request, 'coreapp/report_edit.html', context)
 def report_update(request, id):
-    pass
+    report = Report.objects.get(id=id)
+    form = ReportForm(request.POST, request.FILES, instance=report)
+    if form.is_valid():
+        form.save()
+        return redirect('report-list')
 def report_delete(request, id):
     pass
 
@@ -114,9 +145,15 @@ def relatedfile_create(request):
     context = {'form_relatedfile': form, 'page_title': 'R&D EPI | Create new related file'}
     return render (request, 'coreapp/relatedfile_create.html', context)
 def relatedfile_edit(request, id):
-    pass
+    relatedfile = RelatedFile.objects.get(id=id)
+    form = RelatedFileForm(instance=relatedfile)
+    context = {'relatedfile': relatedfile, 'form_relatedfile': form, 'page_title': 'R&D EPI | Edit related file'}
 def relatedfile_update(request, id):
-    pass
+    relatedfile = RelatedFile.objects.get(id=id)
+    form = RelatedFileForm(request.POST, request.FILES, instance=relatedfile)
+    if form.is_valid():
+        form.save()
+        return redirect('relatedfile-list')
 def relatedfile_delete(request, id):
     pass
 
